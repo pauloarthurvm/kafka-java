a27ac3c690accf82db4761c3c35c4e2407336418
Paulo Varassin <pauloarthurvm@hotmail.com>
2021-10-30 17:53:31 -0300

    Producer config

--- project-1/src/main/java/com/kafka/tutorial/ProducerDemo.java
+++ project-1/src/main/java/com/kafka/tutorial/ProducerDemo.java
@@ -1,9 +1,42 @@
 package com.kafka.tutorial;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.protocol.types.Field;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+import static org.apache.kafka.clients.producer.ProducerConfig.*;
+
 public class ProducerDemo {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        String bootstrapServers = "127.0.0.1:9092";
+
+//        Producer properties
+        Properties properties = new Properties();
+        properties.setProperty(BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+//        Create producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+//        Create a producer record
+        ProducerRecord<String, String> record =
+                new ProducerRecord<String, String>("first_topic", "hello world");
+
+//        Send data
+        producer.send(record);
+
+        producer.flush();
+//        Close does flush AND close
+        producer.close();
+
+
     }
 
 }
